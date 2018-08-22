7fe2669fa3380c1d95d0b9f2553ea515a7027f91
Felipe Sales <sales.felipeb@gmail.com>
2018-08-21 23:20:47 -0300

    Finish mongo setup

--- index.js
+++ index.js
@@ -68,8 +68,3 @@ app.post("/api/confessions", function(req, res) {
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
-
-// const port = process.env.PORT || 5000;
-// app.listen(port);
-
-// console.log(`Listening on ${port}`);
