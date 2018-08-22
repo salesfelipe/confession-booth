09152357194268ba89081a2e4a871a0bcd0e3207
Felipe Sales <sales.felipeb@gmail.com>
2018-08-21 23:03:14 -0300

    remove warning from console

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 
 let db = null
 
-mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
+mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", { useNewUrlParser: true }, function (err, client) {
   if (err) {
     console.log(err);
     process.exit(1);
